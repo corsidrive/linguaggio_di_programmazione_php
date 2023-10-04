310aa4d72c038228a07bba55e7cb0e6f0ce22533
Mona Lisa <sarassoroberto@gmail.com>
2023-10-04 17:46:21 +0200

    Elenco delle opere senza link

--- basi_php/05_galleria_arte_moderna.php
+++ basi_php/05_galleria_arte_moderna.php
@@ -3,16 +3,33 @@
  * Scrivere un elenco non ordinato (ul/li)  dei titoòi dell opere in grassetto
  * autore subito dopo tra parentesi 
  * <strong>Giconda</strong> (500)
- * 
  */
 # Client
 $opere_string = file_get_contents("https://gestione.fondazionetorinomusei.it/media/opendata/COLLEZIONI_MAO.jos.json");
 
-$opere_json = json_decode($opere_string,true);
+$opere_array = json_decode($opere_string,true);
 
-echo count($opere_json);
-echo "<br>";
-print_r($opere_json[3]);
+// echo count($opere_array);
+// echo "<br>";
+// print_r($opere_array[3]);
 ?>
-
-<img src="http://93.62.170.226/foto/mao/878_Hb20.jpg">
+<!-- <img src="http://93.62.170.226/foto/mao/878_Hb20.jpg"> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galleria MAO Elenco delle opere</title>
+</head>
+<body>
+    <h1 class="opere--header"> Elenco delle opere del MAO </h1>
+    <p class="opere--description">
+        Sono presenti: <?= count($opere_array) ?> opere.
+    </p>
+    <ul>
+        <?php foreach ($opere_array as $opera) { ?>
+            <li> <strong> <?= $opera['Titolo'] ?> </strong> (<?= $opera['Datazione'] ?>) </li>
+        <?php } ?>
+    </ul>    
+</body>
+</html>
