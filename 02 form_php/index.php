df9257219733d8dfef89d899612f819a9901c6b4
Mona Lisa <sarassoroberto@gmail.com>
2023-10-11 14:39:51 +0200

    form allievo  passatempi e materie

--- 02 form_php/index.php
+++ 02 form_php/index.php
@@ -8,15 +8,19 @@
 <body>
     <form action="destinazione_del_form.php" method="GET">
         <fieldset>
+            <legend>Dati Anagrafici</legend>
             <label for="nome">nome di battesimo<label>
             <input  name="nome" id="nome"  type="text">
            
             <label  for="cognome" >cognome<label>
    
+            <!-- $_POST | $_GET['nome']  = "Mario" -->
             <input  name="cognome" id="cognome"  type="text">
-
-            <br>
-
+        </fieldset>
+            
+        
+        <fieldset>    
+            <legend>Passatempi</legend>
             <input id="tennis" type="checkbox" value="tennis" name="passatempi[]"> 
             <label for="tennis">Tennis</label>  <br>
             
@@ -25,9 +29,22 @@
 
             <input id="cinema" type="checkbox" value="cinema" name="passatempi[]"> 
             <label for="cinema">Cinema</label>  <br>
-            <input type="submit">
         </fieldset>
 
+        <fieldset>
+            <legend>Materie Scolastiche</legend>
+            <p>Seleziona le tue materia preverita (1 sola )</p>
+
+            <input type="radio" id="php" value="php" name="materie_scolastiche" >
+            <label for="php">Linguaggio di Programmazione  PHP</label>
+            <br>
+
+            <input type="radio" id="javascript" value="javascript" name="materie_scolastiche" >
+            <label for="javascript">Linguaggio di Programmazione  Javascript</label>
+            <br>
+
+        </fieldset>
+        <input type="submit">
     </form>
 </body>
 </html>
