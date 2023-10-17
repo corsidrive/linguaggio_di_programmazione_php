421cf63302e4b3229ceac31fe9bbf1172af7465f
Mona Lisa <sarassoroberto@gmail.com>
2023-10-17 17:29:52 +0200

    cerca

--- 03 le funzioni/funzione_cerca.php
+++ 03 le funzioni/funzione_cerca.php
@@ -6,13 +6,14 @@ function cerca_allievo(string $search,array $allievi):array {
 
     foreach ($allievi as  $allievo) {
         // martino    ===  martino
-
+        
         //  $nome = $allievo['nome'];
         //  $nome_lower = strtolower($nome);
 
         //  $search_lower = strtolower($search);
         //  $search_trim = trim($search_lower);
         //  if($nome_lower === $search_trim) {};
+
 
         if(strtolower($allievo['nome']) === trim(strtolower($search))){
             // array_push($result, $allievo);
