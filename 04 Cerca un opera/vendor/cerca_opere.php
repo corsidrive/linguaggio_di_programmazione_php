081cd924fd302108d37d46b52ad4213d401e639e
Mona Lisa <sarassoroberto@gmail.com>
2023-10-18 11:23:17 +0200

    cerca_chiave

--- 04 Cerca un opera/vendor/cerca_opere.php
+++ 04 Cerca un opera/vendor/cerca_opere.php
@@ -9,4 +9,13 @@ function cerca_opera($search,$opere) {
     }
     return $result;
 }
+function cerca_chiave($search,$key,$opere) {
+    $result = [];
+    foreach ($opere as $opera) {
+        if(trim(strtolower($opera[$key])) === strtolower(trim($search))){
+            $result[] = $opera; 
+        }       
+    }
+    return $result;
+}
 
