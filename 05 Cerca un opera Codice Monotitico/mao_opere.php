a5d55d34f0b391b1781601517d17340fc97e296f
Mona Lisa <sarassoroberto@gmail.com>
2023-10-27 10:11:42 +0200

    correzioni Grammatica

--- 05 Cerca un opera Codice Monotitico/mao_opere.php
+++ 05 Cerca un opera Codice Monotitico/mao_opere.php
@@ -24,7 +24,7 @@ $opere_trovate = $opere_array;
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Oprere MAO</title>
+    <title>Opere MAO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
@@ -32,7 +32,7 @@ $opere_trovate = $opere_array;
         <h1>Museo</h1>
     </header>
     <section id="page__section" class="px-2">
-        <p>Oprere <acronim>MAO</acronim></p>
+        <p>Opere <acronim>MAO</acronim></p>
     </section>
     <main>
         <div class="container">
