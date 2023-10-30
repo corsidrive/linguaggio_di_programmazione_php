765df1f8725e00528ae43db3d74c517553eebdb1
Mona Lisa <sarassoroberto@gmail.com>
2023-10-30 14:44:04 +0100

    gAM OK

--- 06 Cerca un opera non monolitico.php/opere_gam.php
+++ 06 Cerca un opera non monolitico.php/opere_gam.php
@@ -4,17 +4,14 @@ require_once "./vendor/search_services.php";
 require_once "./vendor/fetch_opere_services.php"; 
 require_once "./vendor/opere_views.php"; 
 
-$opere = ottieni_opere("https://gestione.fondazionetorinomusei.it/media/opendata/COLLEZIONI_MAO.jos.json");
-$opera_da_cercare = $_REQUEST['search_word'];
+$opere = ottieni_opere("./resources/COLLEZIONI_GAM.json");
 
-$opere_trovate = cerca_opera($opera_da_cercare,$opere);
-
-$page['page__title'] = "Ricerca Museo";
+$page['page__title'] = "Opere della GAM (Galleria d'arte moderna)";
 $page['page__header'] = "Fondazione Torino Musei";
-$page['page__section'] = "hai cercato: <strong>$opera_da_cercare</strong>";
+$page['page__section'] = $page['page__title'];
 
 get_header($page);
-opere_table($opere_trovate);
+opere_table($opere);
 get_footer($page);
 ?>
 
