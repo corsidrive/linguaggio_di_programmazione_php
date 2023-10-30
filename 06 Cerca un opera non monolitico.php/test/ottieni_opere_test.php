8918a9991e35555a75879dfaf1eac78fd4c37a08
Mona Lisa <sarassoroberto@gmail.com>
2023-10-30 14:24:14 +0100

    JSON GAM FIXED

---
+++ 06 Cerca un opera non monolitico.php/test/ottieni_opere_test.php
@@ -0,0 +1,9 @@
+<?php 
+require_once("../vendor/fetch_opere_services.php");
+
+$path_json = "../resources/COLLEZIONI_GAM.json";
+var_dump(file_exists($path_json));
+
+$result = ottieni_opere($path_json);
+
+var_dump($result);
--- 06 Cerca un opera non monolitico.php/vendor/fetch_opere_services.php
+++ 06 Cerca un opera non monolitico.php/vendor/fetch_opere_services.php
@@ -1,12 +1,20 @@
 <?php 
 
 function ottieni_opere($source){
+   
+  /** TODO: gestire file mancante */
+      $opere_string = file_get_contents($source);
+    //   print_r($opere_string);
+      $opere_array = json_decode($opere_string,true);
+  
+      if(json_last_error() != JSON_ERROR_NONE){
+        echo json_last_error_msg();
+      }
+  
+      return $opere_array;
+  
     # Procurami l'elenco delle opere
 
-    /** TODO: gestire file mancante */
-    $opere_string = file_get_contents($source);
-    $opere_array = json_decode($opere_string,true);
 
-    return $opere_array;
 }
 
